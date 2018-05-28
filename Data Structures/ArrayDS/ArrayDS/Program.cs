7cd9f61ec3837fc4a11fcd694883c752a83d8740
William <bcon615@gmail.com>
2018-05-28 12:14:55 -0400

    Initial Commit

---
+++ Data Structures/ArrayDS/ArrayDS/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+
+    /*
+     * Complete the reverseArray function below.
+     */
+    static int[] reverseArray(int[] a)
+    {
+        /*
+         * Write your code here.
+         */
+
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arrCount = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        int[] res = reverseArray(arr);
+
+        textWriter.WriteLine(string.Join(" ", res));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
