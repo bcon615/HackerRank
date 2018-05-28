f276cf552ddef750f0268b508a778a86655f7fd2
William <bcon615@gmail.com>
2018-05-28 13:00:24 -0400

    Final submission
    Iterate through only half of array

--- Data Structures/ArrayDS/ArrayDS/Program.cs
+++ Data Structures/ArrayDS/ArrayDS/Program.cs
@@ -11,10 +11,14 @@ class Solution
      */
     static int[] reverseArray(int[] a)
     {
-        /*
-         * Write your code here.
-         */
+        for (int i = 0; i < a.Length / 2; i++)
+        {
+            int tmp = a[i];
+            a[i] = a[a.Length - i - 1];
+            a[a.Length - i - 1] = tmp;
+        }
 
+        return a;
     }
 
     static void Main(string[] args)
